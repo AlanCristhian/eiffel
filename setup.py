47a25d19b00529d8975564fe21e1634fdcc78eec
unknown <alan.cristh@gmail.com>
2019-03-18 01:07:48 -0300

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="eiffel",
-    version="0.0.6",
+    version="0.0.7",
     py_modules=["eiffel"],
 
     zip_safe=True,
@@ -10,6 +10,6 @@ setup(
     author_email="alan.cristh@gmail.com",
     description="Another Python Design By Contract implementation.",
     license="MIT",
-    keywords="data structure",
+    keywords="design by contract",
     url="https://github.com/AlanCristhian/eiffel",
 )
